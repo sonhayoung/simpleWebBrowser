a447efffb00779ce0a7be210a72aa76249e3398d
HaYoung Son <thsgkdud123@naver.com>
2016-06-02 20:57:05 +0900

    #2 Event Programming

--- app/src/main/java/kr/hs/emirim/thsgkdud123/simplewebbrowser/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/thsgkdud123/simplewebbrowser/MainActivity.java
@@ -3,7 +3,9 @@ package kr.hs.emirim.thsgkdud123.simplewebbrowser;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -20,10 +22,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         butPrev=(Button)findViewById(R.id.but_prev);
         web=(WebView)findViewById(R.id.web);
         butMove.setOnClickListener(this);
+        butPrev.setOnClickListener(this);
+        web.setWebViewClient(new WebViewClient());
+        WebSettings webSet=web.getSettings();
+        webSet.setBuiltInZoomControls(true);
+        webSet.setJavaScriptEnabled(true);
+        web.loadUrl("http://www.naver.com");
+
     }
 
     @Override
     public void onClick(View v) {
-
+        String url=null;
+        switch(v.getId()){
+            case R.id.but_move:
+                web.loadUrl(editUrl.getText().toString());
+                if(url.contains("http"))
+                    url="http://"+url;
+                    web.loadUrl(url);
+                break;
+            case R.id.but_prev:
+                web.goBack();
+                break;
+        }
     }
 }
